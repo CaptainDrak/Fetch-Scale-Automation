4cd6d5bbdbfaf49c3ce6c5d3fdbb742ecc9910bb
CaptainDrak <ccrinbama@gmail.com>
2021-03-10 11:40:30 -0600

    clarify report variable and update readme
    Changed results to report_results, again, in the name of clarity. Also added a "common error resolution" section to the readme.

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ This is a coding exercise for Fetch, written in Ruby, utilizing selenium and chr
 
 ## Running the Selenium script
 Clone this repo, navigate to local folder containing it in console/terminal, and run the file with `ruby weighing_game.rb`. The page containing the weighing game will launch, the automation will run, and the number of weighings, a list of the weighings, the alert message text, and the identity of the false bar will all be output in the console/terminal.
+
+## Common Error Resultion
+- Your current version of Chrome may need to be forced to update from within the browser settings itself.
+- Chromium browser won't work in place of Chrome; if you are running into driver initialization errors and have Chromium browser installed, you'll need to install Chrome proper.
+- Linux and Mac will handle Ruby PATH addition automagically pretty well, but the Windows Ruby installer sometimes populates the wrong location to your PATH. If you are having issues running ruby commands on Windows console, make sure what it auto-added to your PATH actually matches the reality of where Ruby is installed on your machine.
--- weighing_game.rb
+++ weighing_game.rb
@@ -3,28 +3,28 @@ require "selenium-webdriver"
 driver = Selenium::WebDriver.for :chrome
 driver.get 'http://ec2-54-208-152-154.compute-1.amazonaws.com/'
 
-# Outputs false bar, weighings, number of weighings, and alert message.
-define_method(:report) do |falsebar|
+# Outputs false bar (passed argument), weighings, number of weighings, and alert message.
+define_method(:report_results) do |falsebar|
     puts "It took #{driver.find_elements(xpath: "//li").length} weighings to find the false bar, and it was bar #{falsebar}.\n\nThe weighings were:"
     puts driver.find_element(xpath: '/html/body/div/div/div[1]/div[5]/ol').text
     driver.find_element(id: "coin_#{falsebar}").click
     puts "\nThe alert message was: '#{driver.switch_to.alert.text}'"
 end
 
-# Sends two values from the group of three we know to contain the false bar, checks the
+# Sends two values from the group of three (passed arguments) we know to contain the false bar, checks the
 # results, identifies the false bar, and gathers the list of weighings and alert text.
-define_method(:compare) do |bar0, bar1, bar2|
+define_method(:compare_bars) do |bar0, bar1, bar2|
     driver.find_element(xpath: '/html/body/div/div/div[1]/div[4]/button[1]').click
     driver.find_element(id: "left_0").send_keys bar0
     driver.find_element(id: "right_0").send_keys bar1
     driver.find_element(id: "weigh").click
     weighing_result = driver.find_element(xpath: '/html/body/div/div/div[1]/div[2]').text
     if weighing_result[-1] == '='
-        report(bar2)
+        report_results(bar2)
     elsif weighing_result[-1] == '<'
-        report(bar0)
+        report_results(bar0)
     elsif weighing_result[-1] == '>'
-        report(bar1)
+        report_results(bar1)
     end
 end
 
@@ -39,10 +39,10 @@ sleep 1
 
 # Runs the method to get the specific false bar on the appropriate group of three bars, and weighings and alert text.
 if weighing_result[-1] == '='
-    compare('6', '7', '8')
+    compare_bars('6', '7', '8')
 elsif weighing_result[-1] == '<'
-    compare('0', '1', '2')
+    compare_bars('0', '1', '2')
 elsif weighing_result[-1] == '>'
-    compare('3', '4', '5')
+    compare_bars('3', '4', '5')
 end
 sleep 3
